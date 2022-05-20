a786c59ba4f03b607a216380eb274f170547f5c5
JulianaMFelipe <julianamatosfelipe@gmail.com>
2022-05-19 21:53:13 -0300

    Inicializando projeto

---
+++ src/test/java/com/cadastro/aluno/CadastroAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
